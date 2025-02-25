c7bb34f8f768d7cfc5a594ddb6570d3b3622cd93
Caio Nishijo <cknishijo1@gmail.com>
2025-02-25 20:16:13 -0300

    Update cabecalho.blade.php

--- exercicios/resources/views/cabecalho.blade.php
+++ exercicios/resources/views/cabecalho.blade.php
@@ -12,6 +12,45 @@
                     <li class="nav-item">
                         <a class="nav-link active" href="/exercicios/ex02/ex2">Exercício 2</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex03/ex3">Exercício 3</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex04/ex4">Exercício 4</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex05/ex5">Exercício 5</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex06/ex6">Exercício 6</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex07/ex7">Exercício 7</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex08/ex8">Exercício 8</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex09/ex9">Exercício 9</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex10/ex10">Exercício 10</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex011/ex11">Exercício 11</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex12/ex12">Exercício 12</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex13/ex13">Exercício 13</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex14/ex14">Exercício 14</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/exercicios/ex15/ex15">Exercício 15</a>
+                    </li>
                 </ul>
             </div>
         </div>
