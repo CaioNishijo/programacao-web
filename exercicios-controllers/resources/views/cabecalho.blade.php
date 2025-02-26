040fbbbc68cca36d48835decc396fc78fa31f4d2
CaioNishijo <110948758+CaioNishijo@users.noreply.github.com>
2025-02-25 21:58:50 -0300

    Update cabecalho.blade.php

--- exercicios-controllers/resources/views/cabecalho.blade.php
+++ exercicios-controllers/resources/views/cabecalho.blade.php
@@ -6,50 +6,27 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex01/ex1">Exercício 1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex02/ex2">Exercício 2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex03/ex3">Exercício 3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex04/ex4">Exercício 4</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex05/ex5">Exercício 5</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex06/ex6">Exercício 6</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex07/ex7">Exercício 7</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex08/ex8">Exercício 8</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex09/ex9">Exercício 9</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex10/ex10">Exercício 10</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex011/ex11">Exercício 11</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex12/ex12">Exercício 12</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex13/ex13">Exercício 13</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex14/ex14">Exercício 14</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/exercicios/ex15/ex15">Exercício 15</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Exercícios
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/exercicios/ex01/ex1">Exercício 1</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex02/ex2">Exercício 2</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex03/ex3">Exercício 3</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex04/ex4">Exercício 4</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex05/ex5">Exercício 5</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex06/ex6">Exercício 6</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex07/ex7">Exercício 7</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex08/ex8">Exercício 8</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex09/ex9">Exercício 9</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex10/ex10">Exercício 10</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex011/ex11">Exercício 11</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex12/ex12">Exercício 12</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex13/ex13">Exercício 13</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex14/ex14">Exercício 14</a></li>
+                            <li><a class="dropdown-item" href="/exercicios/ex15/ex15">Exercício 15</a></li>
+                        </ul>
                     </li>
                 </ul>
             </div>
